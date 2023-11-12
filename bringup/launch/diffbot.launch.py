dbd4155b96c3685a0f0e7b26a56188dca84afa02
kenmacken <kenmacken@gmail.com>
2023-11-12 12:42:27 +1100

    Updated urdf filenames

--- bringup/launch/diffbot.launch.py
+++ bringup/launch/diffbot.launch.py
@@ -50,7 +50,7 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare("ros2_ddsm115"), "urdf", "diffbot.urdf.xacro"]
+                [FindPackageShare("ros2_ddsm115"), "urdf", "core.urdf.xacro"]
             ),
             " ",
             "use_mock_hardware:=",
--- bringup/launch/rsp.launch.py
+++ bringup/launch/rsp.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('ros2_ddsm115'))
-    xacro_file = os.path.join(pkg_path,'urdf','diffbot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path,'urdf','core.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
